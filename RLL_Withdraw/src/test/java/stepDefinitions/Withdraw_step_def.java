1263b5c97d85f0d6d48e9fb1c49e4c48bb3764a0
chozha <139967475+Chozha-njr@users.noreply.github.com>
2024-01-20 22:58:20 +0530

    Withdraw_step_def.java

--- RLL_Withdraw/src/test/java/stepDefinitions/Withdraw_step_def.java
+++ RLL_Withdraw/src/test/java/stepDefinitions/Withdraw_step_def.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
-//import com.fasterxml.jackson.databind.deser.Deserializers.Base;
+
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -153,105 +153,4 @@ public class Withdraw_step_def {
 }
 
 	
-	/*@Then("check withdraw with 'o' value")
-	public void Exceed_bankamount_fails() {
-		logger.info("check withdraw with 'o' value");
-		
-		Withdraw_Method.navigateToURL("http://dbankdemo.com/bank/login");
-		Withdraw_Method.performLogin("kirans@gmail.com", "Kiran4301@S");
-		Withdraw_Method.clickWithdrawLink();
-		
-		Withdraw_Method.clickselectaccount();
-		Withdraw_Method.invalid_input_0("0");
-		String expected="The withdraw amount  must be greater than $0.00";
-		String actual=Withdraw_Method.Error_0();
-		actual.equals(expected); 
-		Assert.assertTrue(actual.contains(expected),"please type valid input");
 	
-}}
-
-
-	/*try {
-	            String[] errorXPaths = {
-	                    "//span[contains(text(),'The withdraw amount ($0.00) must be greater than $')]", 
-	                    "//span[contains(text(),'The withdraw amount ($1000000000000000.00) is grea')]", 
-	                    "//div[@id='errorMessageBox3']/span"
-	            };
-	            String firstErrorMessage = null;
-
-	            for (String errorXPath : errorXPaths) {
-	                By errorLocator = By.xpath(errorXPath);
-
-	                try {
-	                    WebElement errorMessageElement = driver.findElement(errorLocator);
-
-	                    if (errorMessageElement.isDisplayed()) {
-	                        // Capture the first error message and break the loop
-	                        firstErrorMessage = errorMessageElement.getText();
-	                        throw new AssertionError("Error: " + firstErrorMessage);
-	                    }
-	                } catch (org.openqa.selenium.NoSuchElementException e) {
-	                    // Handle the case when the element is not found (error message not displayed)
-	                    
-	                }
-	            }
-
-	            // If no error message is displayed, you can run specific code
-	            if (firstErrorMessage == null) {
-	                System.out.println("No error message displayed. Running additional code...");
-	            } else {
-	                // If an error message is displayed, perform an assertion
-	                Assert.fail("Error: " + firstErrorMessage);
-	            }
-
-	            // Perform additional actions or verifications
-	            // ...
-
-	        } catch (AssertionError e) {
-	            // Handle the assertion error
-	        	
-	            System.out.println("Assertion failed: " + e.getMessage());
-	        }
-	}
-	22222
-	@Then("check withdraw with invalid amount")
-	public void invalid_input() {
-		logger.info("check withdraw with invalid amount");
-		Withdraw_Method.clickselectaccount();
-		Withdraw_Method.invalid_input_0("0");
-		String expected="The withdraw amount ($0.00) must be greater than $0.00";
-		String actual=Withdraw_Method.Error_0();
-		actual.equals(expected); 
-		Assert.assertTrue(actual.contains(expected));
-		
-		
-		
-		
-	}
-	
-	@Then("check withdraw with exceed bank amount")
-	public void Exceed_bankamount() {
-		logger.info("check withdraw with exceed bank amount");
-		Withdraw_Method.clickselectaccount();
-		Withdraw_Method.exceedbalance("1000000000000000.00");
-		String expected="The withdraw amount ($1000000000000000.00) is greater than the available balance ($5999999998641.00) and overdraft limit ($25.00).";
-		String actual=Withdraw_Method.Error_exceed_amount();
-		actual.equals(expected); 
-		Assert.assertTrue(actual.contains(expected));
-		
-		
-	}
-	
-	
-	@Then("give valid amount and deposite")
-	public void Withdraw_Amount_from_account() {
-		logger.info("give valid amount and deposite");
-		try {
-			Withdraw_Method.clickselectaccount();
-			Withdraw_Method.withdrawcheck("120");
-        } catch (Exception ex) {
-        	Assert.fail("Withdraw failed");
-        }
-
-	*/
-    		
